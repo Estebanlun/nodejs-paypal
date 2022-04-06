c464d25e41326e6eaa0d858e9fbfac80c6b2c97b
Esteban Luna <estebanlun14777@gmail.com>
2022-04-06 13:37:02 -0300

    first commit

---
+++ src/controllers/payments.conroller.js
@@ -0,0 +1,93 @@
+import axios from "axios";
+import { PAYPAL_API, PAYPAL_API_CLIENT, PAYPAL_API_SECRET } from "../config";
+
+export const createOrder = async (req, res) => {
+  try {
+    const order = {
+      intent: "CAPTURE",
+      purchase_units: [
+        {
+          amount: {
+            currency_code: "USD",
+            value: "102.00",
+          },
+          description: "Order for the item",
+        },
+      ],
+      application_context: {
+        brand_name: "Payment",
+        landing_page: "BILLING",
+        user_action: "PAY_NOW",
+        return_url: "http://localhost:3000/capture-order",
+        cancel_url: "http://localhost:3000/cancel-order",
+      },
+    };
+
+    const params = new URLSearchParams();
+    params.append("grant_type", "client_credentials");
+
+    const {
+      data: { access_token },
+    } = await axios.post(
+      "https://api-m.sandbox.paypal.com/v1/oauth2/token",
+      params,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        auth: {
+          username: PAYPAL_API_CLIENT,
+          password: PAYPAL_API_SECRET,
+        },
+      }
+    );
+
+    console.log(access_token);
+
+    const response = await axios.post(
+      `${PAYPAL_API}/v2/checkout/orders`,
+      order,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+
+    console.log(response.data);
+
+    return res.json(response.data);
+
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send("Something went wrong");
+  }
+};
+
+export const captureOrder = async (req, res) => {
+  const { token } = req.query;
+
+  try {
+    const response = await axios.post(
+      `${PAYPAL_API}/v2/checkout/orders/${token}/capture`,
+      {},
+      {
+        auth: {
+          username: PAYPAL_API_CLIENT,
+          password: PAYPAL_API_SECRET,
+        },
+      }
+    );
+
+    console.log(response.data);
+
+    res.redirect("/payed.html");
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: "Internal Server error" });
+  }
+};
+
+export const cancelOrder = async (req, res) => {
+  res.redirect("/");
+};
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
+import { PORT } from "./config";
+
+import paymentRoutes from "./routes/payment.routes";
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use(paymentRoutes);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log("Server on port", PORT);
+});
+
