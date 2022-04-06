7384626f65cc21322ea9770264007ae70a612ce6
Esteban Luna Claraso <84942993+Estebanlun@users.noreply.github.com>
2022-04-06 13:37:33 -0300

    Delete config.js

--- src/config.js
+++
@@ -1,4 +0,0 @@
-export const PAYPAL_API_CLIENT = "AcUZ6FghmS-GQ_oCgoB61niib-SWBn7h8jvFHq1meUpmG74JexSgplLzwUpLH_zNvcRQakdlzaw6UBU8";
-export const PAYPAL_API_SECRET = "EFY9fpK5YXtLy4uJQnbb0TDJEFB1ZnClToIQ5sUVmMFv6rz4J4rAFuhs0pCIsUO12SCixJjQQVLiZmP9";
-export const PAYPAL_API = "https://api-m.sandbox.paypal.com";
-export const PORT = 3000;
